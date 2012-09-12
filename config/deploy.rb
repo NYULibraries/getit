0e21d4e9fc73a0d22bda4072936f34ddec521364
Scot Dalton <scotdalton@gmail.com>
2012-09-12 09:37:40 -0400

    Capify, update/add expected results for request testing, and remove unnecessary files from the repository.

---
+++ config/deploy.rb
@@ -0,0 +1,52 @@
+# Multistage
+require 'capistrano/ext/multistage'
+# Load bundler-capistrano gem
+require "bundler/capistrano"
+# Load rvm-capistrano gem
+require "rvm/capistrano"
+
+set :ssh_options, {:forward_agent => true}
+set :app_title, "getit"
+set :application, "#{app_title}_repos"
+
+# RVM  vars
+set :rvm_ruby_string, "1.9.3-p125"
+set :rvm_type, :system
+
+# Bundle vars
+
+# Git vars
+set :repository, "git@github.com:NYULibraries/getit.git" 
+set :scm, :git
+set :scm_username, "Jenkins-NYULib"
+set :deploy_via, :remote_cache
+set(:branch, 'master') unless exists?(:branch)
+set :git_enable_submodules, 1
+
+# Environments
+set :stages, ["staging", "production"]
+set :default_stage, "staging"
+set :keep_releases, 5
+set :use_sudo, false
+
+# Rails specific vars
+set :normalize_asset_timestamps, false
+
+namespace :deploy do
+  desc "Start Application"
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+  task :passenger_symlink do
+    run "rm -rf /apps/#{app_title} && ln -s #{current_path}/public /apps/#{app_title}"
+  end
+end
+
+after "deploy", "deploy:cleanup", "deploy:passenger_symlink"
---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+server "umlaut1.bobst.nyu.edu", :app, :web, :db, :primary => true
+server "umlaut2.bobst.nyu.edu", :app, :web
+set :deploy_to, "/apps/#{application}"
+set :user, "wsops"
