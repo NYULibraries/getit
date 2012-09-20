a41b9fc1f2305771db0edabacae16ea09e4adf4d
Scot Dalton <scotdalton@gmail.com>
2012-09-20 15:03:54 -0400

    Updated searcher for sorting and pagination.

--- app/controllers/search_methods/sfx4_solr/searcher.rb
+++ app/controllers/search_methods/sfx4_solr/searcher.rb
@@ -11,7 +11,7 @@ module SearchMethods
       module InstanceMethods
         protected
         def find_by_title
-          _find_by_title(title_query_param, search_type_param, context_object_from_params)
+          _find_by_title(title_query_param, search_type_param, context_object_from_params, param[:page])
         end
       
         def find_by_group
@@ -19,16 +19,28 @@ module SearchMethods
         end
 
         private
-        def _find_by_title(query_param, search_type, context_object)
+        def _find_by_title(query_param, search_type, context_object, page=1)
           Rails.logger.warn az_title_klass.to_s
           query = az_title_klass.connection.quote_string(query_param)
           titles = case search_type
             when "contains"
-              az_title_klass.search {fulltext query}.results
+              az_title_klass.search {
+                fulltext query
+                order_by(:title_sort, :asc)
+                paginate(:page => page, :per_page => 20)
+              }.results
             when "begins"
-              az_title_klass.search {with(:title_exact).starts_with(query)}.results
+              az_title_klass.search {
+                with(:title_exact).starts_with(query)
+                order_by(:title_sort, :asc)
+                paginate(:page => page, :per_page => 20)
+              }.results
             else # exact
-              az_title_klass.search {with(:title_exact, query)}.results
+              az_title_klass.search {
+                with(:title_exact, query)
+                order_by(:title_sort, :asc)
+                paginate(:page => page, :per_page => 20)
+              }.results
             end   
           return [titles.map{|title| title.to_context_object context_object}, titles.count]
         end
