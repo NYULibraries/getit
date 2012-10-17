69e843023daa71d234ab04da16cfef94fd633bdc
Scot Dalton <scotdalton@gmail.com>
2012-10-17 10:47:31 -0400

    SFX4 models need to check their connections.

--- app/models/sfx4/cu/base.rb
+++ app/models/sfx4/cu/base.rb
@@ -1,10 +1,18 @@
 module Sfx4
   module Cu
     class Base < ActiveRecord::Base
-      self.establish_connection :sfx4_cu
+      # Was a SFX DB connection set in database.yml to connect directly to sfx?
+      def self.connection_configured?
+        config = ActiveRecord::Base.configurations["sfx4_cu"]
+        (not (config.nil? or config.blank? or config["adapter"].blank?))
+      end
+
+      self.establish_connection :sfx4_cu if self.connection_configured?
+
       # ActiveRecord likes it when we tell it this is an abstract
       # class only. 
       self.abstract_class = true 
+
       extend Sfx4::Abstract::Base
 
       # All SFX things are read-only!
--- app/models/sfx4/ns/base.rb
+++ app/models/sfx4/ns/base.rb
@@ -1,10 +1,18 @@
 module Sfx4
   module Ns
     class Base < ActiveRecord::Base
-      self.establish_connection :sfx4_ns
+      # Was a SFX DB connection set in database.yml to connect directly to sfx?
+      def self.connection_configured?
+        config = ActiveRecord::Base.configurations["sfx4_ns"]
+        (not (config.nil? or config.blank? or config["adapter"].blank?))
+      end
+
+      self.establish_connection :sfx4_ns if self.connection_configured?
+
       # ActiveRecord likes it when we tell it this is an abstract
       # class only. 
       self.abstract_class = true 
+
       extend Sfx4::Abstract::Base
 
       # All SFX things are read-only!
