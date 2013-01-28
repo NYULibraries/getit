6d393a67ced5014006cfbd7a9b56313da5238d3e
Scot Dalton <scotdalton@gmail.com>
2013-01-28 18:32:02 -0500

    Add institutional tests for resolve.

--- test/functional/resolve_controller_test.rb
+++ test/functional/resolve_controller_test.rb
@@ -41,6 +41,44 @@ class ResolveControllerTest < ActionController::TestCase
     end
   end
 
+  test "resolve request available not logged in NS" do
+    umlaut_request = requests(:primo_book_virtual_inequality)
+    VCR.use_cassette('resolve request available not logged in NS') do
+      get(:index, {'umlaut.request_id' => umlaut_request.id, "umlaut.institution" => "NS"})
+      assert_response :success
+      assert_select 'nav.umlaut-resolve-nav div#permalink span', {:count => 1, :text => "URL:"}
+      assert_select 'nav.umlaut-resolve-nav div#permalink a', {:count => 1, 
+        :text => "http://test.host/go/#{umlaut_request.id}?umlaut.institution=NS", :href => "http://test.host/go/#{umlaut_request.referent.id}?umlaut.institution=NS"}
+      assert_select 'nav.umlaut-resolve-nav i.icons-famfamfam-lock_open', 1
+      assert_select 'nav.umlaut-resolve-nav a.login', {:count => 1, :text => "Login", 
+        :href => "http://test.host/login?return_url=http://test.host/go/#{umlaut_request.referent.id}&umlaut.institution=NS"}
+      assert_select 'div.umlaut-holdings div.umlaut-holding ' do |elements|
+        elements.each do |element|
+          assert_select element, "a.ajax_window", {:count => 0}
+        end
+      end
+    end
+  end
+
+  test "resolve request available not logged in CU" do
+    umlaut_request = requests(:primo_book_virtual_inequality)
+    VCR.use_cassette('resolve request available not logged in CU') do
+      get(:index, {'umlaut.request_id' => umlaut_request.id, "umlaut.institution" => "CU"})
+      assert_response :success
+      assert_select 'nav.umlaut-resolve-nav div#permalink span', {:count => 1, :text => "URL:"}
+      assert_select 'nav.umlaut-resolve-nav div#permalink a', {:count => 1, 
+        :text => "http://test.host/go/#{umlaut_request.id}?umlaut.institution=CU", :href => "http://test.host/go/#{umlaut_request.referent.id}?umlaut.institution=CU"}
+      assert_select 'nav.umlaut-resolve-nav i.icons-famfamfam-lock_open', 1
+      assert_select 'nav.umlaut-resolve-nav a.login', {:count => 1, :text => "Login", 
+        :href => "http://test.host/login?return_url=http://test.host/go/#{umlaut_request.referent.id}&umlaut.institution=CU"}
+      assert_select 'div.umlaut-holdings div.umlaut-holding ' do |elements|
+        elements.each do |element|
+          assert_select element, "a.ajax_window", {:count => 0}
+        end
+      end
+    end
+  end
+
   # test "expire old holdings" do
   #   umlaut_request = requests(:primo_book_virtual_inequality)
   #   VCR.use_cassette('expire old holdings') do
