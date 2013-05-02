85d52530fae74b82272dbde34f51b6612a67062a
Scot Dalton <scotdalton@gmail.com>
2013-05-02 10:57:07 -0400

    Don't run staging environment on a sub uri.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,5 @@
 GetIt::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.action_controller.relative_url_root = '/getit'
 
   # Code is not reloaded between requests
   config.cache_classes = true
