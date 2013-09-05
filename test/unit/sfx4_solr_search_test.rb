752fc60ca9db25885de10754dcbe883078bfe276
hab278 <as>
2013-09-05 17:22:32 -0400

    New tests for searches

--- test/unit/sfx4_solr_search_test.rb
+++ test/unit/sfx4_solr_search_test.rb
@@ -1,5 +1,8 @@
+#!/bin/env ruby
+# encoding: utf-8
 require 'test_helper'
 class Sfx4SolrSearchTest < ActiveSupport::TestCase
+  include SearchMethods::Sfx4Solr::Local
   test "Sfx4 Base" do
     if Sfx4::Local::Base.connection_configured?
       assert_equal Sfx4::Local::Base, SearchMethods::Sfx4Solr::Local.sfx4_base
@@ -40,23 +43,9 @@ class Sfx4SolrSearchTest < ActiveSupport::TestCase
     if Sfx4::Local::AzTitle.connection_configured?
       VCR.use_cassette('sfx4solr title contains search') do
         query = "economist"
-        query = query.upcase
-        [/^THE /, /^AN? /].each do |article|
-          query.gsub! article, ""
-        end
-        results = Sfx4::Local::AzTitle.search do
-          keywords query do
-            fields(title: 1.0)
-            boost 10.0 do
-              with(:title_exact, query)
-            end
-          end
-          order_by(:score, :desc)
-          order_by(:title_sort, :asc)
-        end.results
-        assert_instance_of Array, results
-        assert(results.size > 0)
-        assert_equal "The Economist: Blogs", results.first.TITLE_DISPLAY
+        search_type = "contains"
+        first_result = "The Economist: Blogs"
+        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
       end
     end
   end
@@ -65,124 +54,86 @@ class Sfx4SolrSearchTest < ActiveSupport::TestCase
     if Sfx4::Local::AzTitle.connection_configured?
       VCR.use_cassette('sfx4solr title starts with search') do
         query = "economist"
-        query = query.upcase
-        [/^THE /, /^AN? /].each do |article|
-          query.gsub! article, ""
-        end
-        results = Sfx4::Local::AzTitle.search do
-          fulltext query do
-            phrase_fields(title: 2.0)
-            phrase_slop 0
-            boost 10.0 do
-              with(:title_exact).starting_with(query)
-            end
-            boost 5.0 do
-              with(:title_exact).starting_with(query)
-            end
-            boost 5.0 do
-              with(:title_exact, query)
-            end
-          end
-          order_by(:score, :desc)
-          order_by(:title_sort, :asc)
-          
-          paginate(:page => 1, :per_page => 20)
-        end.results
-        assert_instance_of Array, results
-        assert(results.size > 0)
-        assert_equal "The Economist: Blogs", results.first.TITLE_DISPLAY
+        search_type = "begins"
+        first_result = "The Economist: Blogs"
+        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
       end
     end
   end
 
-  test "sfx4solr exact search" do
+  test "sfx4solr title exact search" do
     if Sfx4::Local::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr exact search') do
+      VCR.use_cassette('sfx4solr title exact search') do
         query = "The New Yorker"
-        query = query.upcase
-        [/^THE /, /^AN? /].each do |article|
-          query.gsub! article, ""
-        end
-        results = Sfx4::Local::AzTitle.search do
-          fulltext query do
-            phrase_fields(title: 2.0)
-            phrase_slop 0
-            boost 10.0 do
-              with(:title_exact, query)
-            end
-          end
-          order_by(:score, :desc)
-          order_by(:title_sort, :asc)
-        end.results
-        assert_instance_of Array, results
-        assert(results.size > 0)
-        assert_equal "The Economist: Blogs", results.first.TITLE_DISPLAY
+        search_type = "exact"
+        first_result = "The New - Yorker (1836-1841)‎"
+        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
       end
     end
   end
 
-  test "sfx4solr fulltext search NS" do
+  test "sfx4solr title contains search NS" do
     if Sfx4::Ns::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr fulltext search NS') do
+      VCR.use_cassette('sfx4solr title contains search NS') do
         query = "New York"
-        results = Sfx4::Ns::AzTitle.search {fulltext query}.results
-        assert_instance_of Array, results
-        assert(results.size > 0)
+        search_type = "contains"
+        first_result = "The Economist: Blogs"
+        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
       end
     end
   end
   
-  test "sfx4solr starts with search NS" do
+  test "sfx4solr title starts with search NS" do
     if Sfx4::Ns::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr starts with search NS') do
+      VCR.use_cassette('sfx4solr title starts with search NS') do
         query = "Journal of"
-        results = Sfx4::Ns::AzTitle.search {with(:title_exact).starting_with(query)}.results
-        assert_instance_of Array, results
-        assert(results.size > 0)
+        search_type = "begins"
+        first_result = "The Economist: Blogs"
+        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
       end
     end
   end
   
-  test "sfx4solr exact search NS" do
+  test "sfx4solr title exact search NS" do
     if Sfx4::Ns::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr exact search NS') do
+      VCR.use_cassette('sfx4solr title exact search NS') do
         query = "The New Yorker"
-        results = Sfx4::Ns::AzTitle.search {with(:title_exact, query)}.results
-        assert_instance_of Array, results
-        assert(results.size > 0)
+        search_type = "exact"
+        first_result = "The New - Yorker (1836-1841)‎"
+        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
       end
     end
   end
   
-  test "sfx4solr fulltext search CU" do
+  test "sfx4solr title contains search CU" do
     if Sfx4::Cu::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr fulltext search CU') do
+      VCR.use_cassette('sfx4solr title contains search CU') do
         query = "New York"
-        results = Sfx4::Cu::AzTitle.search {fulltext query}.results
-        assert_instance_of Array, results
-        assert(results.size > 0)
+        search_type = "contains"
+        first_result = "New York"
+        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
       end
     end
   end
 
-  test "sfx4solr starts with search CU" do
+  test "sfx4solr title starts with search CU" do
     if Sfx4::Cu::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr starts with search CU') do
+      VCR.use_cassette('sfx4solr title starts with search CU') do
         query = "Journal of"
-        results = Sfx4::Cu::AzTitle.search {with(:title_exact).starting_with(query)}.results
-        assert_instance_of Array, results
-        assert(results.size > 0)
+        search_type = "begins"
+        first_result = "Journal of accountancy"
+        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
       end
     end
   end
 
-  test "sfx4solr exact search CU" do
+  test "sfx4solr title exact search CU" do
     if Sfx4::Cu::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr exact search CU') do
+      VCR.use_cassette('sfx4solr title exact search CU') do
         query = "The New Yorker"
-        results = Sfx4::Cu::AzTitle.search {with(:title_exact, query)}.results
-        assert_instance_of Array, results
-        assert(results.size > 0)
+        search_type = "exact"
+        first_result = "The New - Yorker (1836-1841)‎"
+        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
       end
     end
   end
