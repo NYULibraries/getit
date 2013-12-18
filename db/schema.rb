519a43e4625b31aee001d9019462cbaf6dad1ef4
Scot Dalton <scotdalton@gmail.com>
2013-12-18 11:08:38 -0500

    Add unique username index to users. Fixes issue #19.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130424135946) do
+ActiveRecord::Schema.define(:version => 20131218153325) do
 
   create_table "clickthroughs", :force => true do |t|
     t.integer  "request_id",          :default => 0, :null => false
@@ -136,5 +136,7 @@ ActiveRecord::Schema.define(:version => 20130424135946) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
+
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
