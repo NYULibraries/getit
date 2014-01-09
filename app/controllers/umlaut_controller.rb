eacbd1d11fbca16b8a32f7cc457925585a327c06
Scot Dalton <scotdalton@gmail.com>
2014-01-08 22:02:53 -0500

    Update help section title and use default font sizes.

--- app/controllers/umlaut_controller.rb
+++ app/controllers/umlaut_controller.rb
@@ -148,6 +148,9 @@ class UmlautController < ApplicationController
     # export_citation[:bg_update] = false
     # export_citation[:visibility] = :complete_with_responses
 
+    help = resolve_sections[resolve_sections.index {|s| s[:div_id].to_s == "help"}]
+    help[:section_title] = "Questions"
+
     document_delivery = resolve_sections[resolve_sections.index {|s| s[:div_id].to_s == "document_delivery"}]
     document_delivery[:section_title] = "Get a copy from Interlibrary Loan (ILL)"
     document_delivery[:section_prompt] = "Please do not use for available or offsite materials."
