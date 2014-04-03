c2f4a2b29e737d9d251ad2171c488c7f00445047
Scot Dalton <scotdalton@gmail.com>
2014-04-03 12:21:27 -0400

    Add holding state examples to the holding specification

--- spec/models/holding_spec.rb
+++ spec/models/holding_spec.rb
@@ -3,6 +3,28 @@ describe Holding do
   let(:service_response) { create(:service_response) }
   subject(:holding) { Holding.new(service_response) }
   it { should be_a Holding }
+  describe '#ill?' do
+    subject { holding.ill? }
+    context 'when it is an ILL service response' do
+      let(:service_response) { build(:ill_service_response) }
+      it { should be_true }
+    end
+    context 'when it is not an ILL service response' do
+      let(:service_response) { build(:available_service_response) }
+      it { should be_false }
+    end
+  end
+  describe '#available?' do
+    subject { holding.available? }
+    context 'when it is an available service response' do
+      let(:service_response) { build(:available_service_response) }
+      it { should be_true }
+    end
+    context 'when it is not an available service response' do
+      let(:service_response) { build(:ill_service_response) }
+      it { should be_false }
+    end
+  end
   context 'when initialized without any arguments' do
     it 'should raise an ArgumentError' do
       expect { Holding.new }.to raise_error ArgumentError
