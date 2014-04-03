97950c6c943b81c2698e82e02a58103288bf084b
Scot Dalton <scotdalton@gmail.com>
2014-04-03 12:04:54 -0400

    Raise an ArgumentError is the holding is created with an object that is not a ServiceResponse

--- spec/models/holding_spec.rb
+++ spec/models/holding_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 describe Holding do
-  let(:service_response) { }
+  let(:service_response) { create(:service_response) }
   subject(:holding) { Holding.new(service_response) }
   it { should be_a Holding }
+  context 'when initialized without any arguments' do
+    it 'should raise an ArgumentError' do
+      expect { Holding.new }.to raise_error ArgumentError
+    end
+  end
+  context 'when initialized with a service response argument' do
+    context 'but the service response argument is not a ServiceResponse' do
+      it 'should raise an ArgumentError' do
+        expect { Holding.new("invalid") }.to raise_error ArgumentError
+      end
+    end
+  end
 end
