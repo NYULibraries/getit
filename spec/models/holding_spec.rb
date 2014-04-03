46de66604ca4d98039cf4bb3cc1476486ebf0f61
Scot Dalton <scotdalton@gmail.com>
2014-04-03 17:07:15 -0400

    Add examples for holding "availability" states

--- spec/models/holding_spec.rb
+++ spec/models/holding_spec.rb
@@ -5,23 +5,281 @@ describe Holding do
   it { should be_a Holding }
   describe '#ill?' do
     subject { holding.ill? }
-    context 'when it is an ILL service response' do
+    context 'when the service response has an processing status' do
+      let(:service_response) { build(:processing_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response has an ILL status' do
       let(:service_response) { build(:ill_service_response) }
       it { should be_true }
     end
-    context 'when it is not an ILL service response' do
+    context 'when the service response has a checked out status' do
+      let(:service_response) { build(:checked_out_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response has a requested status' do
+      let(:service_response) { build(:requested_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response has an on order status' do
+      let(:service_response) { build(:on_order_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response has a billed as lost status' do
+      let(:service_response) { build(:billed_as_lost_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response has an available status' do
       let(:service_response) { build(:available_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an offsite status' do
+      let(:service_response) { build(:offsite_service_response) }
       it { should be_false }
     end
   end
   describe '#available?' do
     subject { holding.available? }
-    context 'when it is an available service response' do
+    context 'when the service response has an processing status' do
+      let(:service_response) { build(:processing_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an ILL status' do
+      let(:service_response) { build(:ill_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a checked out status' do
+      let(:service_response) { build(:checked_out_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a requested status' do
+      let(:service_response) { build(:requested_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an on order status' do
+      let(:service_response) { build(:on_order_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a billed as lost status' do
+      let(:service_response) { build(:billed_as_lost_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an available status' do
       let(:service_response) { build(:available_service_response) }
       it { should be_true }
     end
-    context 'when it is not an available service response' do
+    context 'when the service response has an offsite status' do
+      let(:service_response) { build(:offsite_service_response) }
+      it { should be_false }
+    end
+  end
+  describe '#checked_out?' do
+    subject { holding.checked_out? }
+    context 'when the service response has an processing status' do
+      let(:service_response) { build(:processing_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an ILL status' do
       let(:service_response) { build(:ill_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a checked out status' do
+      let(:service_response) { build(:checked_out_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response hasan requested status' do
+      let(:service_response) { build(:requested_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an on order status' do
+      let(:service_response) { build(:on_order_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a billed as lost status' do
+      let(:service_response) { build(:billed_as_lost_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an available status' do
+      let(:service_response) { build(:available_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an offsite status' do
+      let(:service_response) { build(:offsite_service_response) }
+      it { should be_false }
+    end
+  end
+  describe '#offsite?' do
+    subject { holding.offsite? }
+    context 'when the service response has an processing status' do
+      let(:service_response) { build(:processing_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an ILL status' do
+      let(:service_response) { build(:ill_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a checked out status' do
+      let(:service_response) { build(:checked_out_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response hasan requested status' do
+      let(:service_response) { build(:requested_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an on order status' do
+      let(:service_response) { build(:on_order_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a billed as lost status' do
+      let(:service_response) { build(:billed_as_lost_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an available status' do
+      let(:service_response) { build(:available_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an offsite status' do
+      let(:service_response) { build(:offsite_service_response) }
+      it { should be_true }
+    end
+  end
+  describe '#requested?' do
+    subject { holding.requested? }
+    context 'when the service response has an processing status' do
+      let(:service_response) { build(:processing_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an ILL status' do
+      let(:service_response) { build(:ill_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a checked out status' do
+      let(:service_response) { build(:checked_out_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response hasan requested status' do
+      let(:service_response) { build(:requested_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response has an on order status' do
+      let(:service_response) { build(:on_order_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a billed as lost status' do
+      let(:service_response) { build(:billed_as_lost_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an available status' do
+      let(:service_response) { build(:available_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an offsite status' do
+      let(:service_response) { build(:offsite_service_response) }
+      it { should be_false }
+    end
+  end
+  describe '#recallable?' do
+    subject { holding.recallable? }
+    context 'when the service response has an processing status' do
+      let(:service_response) { build(:processing_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an ILL status' do
+      let(:service_response) { build(:ill_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a checked out status' do
+      let(:service_response) { build(:checked_out_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response hasan requested status' do
+      let(:service_response) { build(:requested_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response has an on order status' do
+      let(:service_response) { build(:on_order_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a billed as lost status' do
+      let(:service_response) { build(:billed_as_lost_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an available status' do
+      let(:service_response) { build(:available_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an offsite status' do
+      let(:service_response) { build(:offsite_service_response) }
+      it { should be_false }
+    end
+  end
+  describe '#processing?' do
+    subject { holding.processing? }
+    context 'when the service response has an processing status' do
+      let(:service_response) { build(:processing_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response has an ILL status' do
+      let(:service_response) { build(:ill_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a checked out status' do
+      let(:service_response) { build(:checked_out_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response hasan requested status' do
+      let(:service_response) { build(:requested_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an on order status' do
+      let(:service_response) { build(:on_order_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a billed as lost status' do
+      let(:service_response) { build(:billed_as_lost_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an available status' do
+      let(:service_response) { build(:available_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an offsite status' do
+      let(:service_response) { build(:offsite_service_response) }
+      it { should be_false }
+    end
+  end
+  describe '#in_processing?' do
+    subject { holding.in_processing? }
+    context 'when the service response has an processing status' do
+      let(:service_response) { build(:processing_service_response) }
+      it { should be_true }
+    end
+    context 'when the service response has an ILL status' do
+      let(:service_response) { build(:ill_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a checked out status' do
+      let(:service_response) { build(:checked_out_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response hasan requested status' do
+      let(:service_response) { build(:requested_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an on order status' do
+      let(:service_response) { build(:on_order_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has a billed as lost status' do
+      let(:service_response) { build(:billed_as_lost_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an available status' do
+      let(:service_response) { build(:available_service_response) }
+      it { should be_false }
+    end
+    context 'when the service response has an offsite status' do
+      let(:service_response) { build(:offsite_service_response) }
       it { should be_false }
     end
   end
