6ef1e8e1499c5d5620b101ab19ee70e9baf6cf7b
Scot Dalton <scotdalton@gmail.com>
2014-04-03 17:06:30 -0400

    Add service response traits for Primo source holding states

--- spec/factories/service_response_factory.rb
+++ spec/factories/service_response_factory.rb
@@ -31,6 +31,15 @@ FactoryGirl.define do
         }
       end
     end
+    trait :available do
+      service_data do
+        {
+          status_code: 'available',
+          status: 'Available',
+          requestability: 'deferred'
+        }
+      end
+    end
     trait :checked_out do
       service_data do
         {
@@ -44,12 +53,63 @@ FactoryGirl.define do
       service_data do
         {
           status_code: 'ill',
-          status: 'Request ILL'
+          status: 'Request ILL',
           requestability: 'yes'
         }
       end
     end
-    factory :checked_out_service_response, traits: [:primo_source, :checked_out]
+    trait :on_order do
+      service_data do
+        {
+          status_code: 'check_holdings',
+          status: 'On Order',
+          requestability: 'yes'
+        }
+      end
+    end
+    trait :billed_as_lost do
+      service_data do
+        {
+          status_code: 'billed_as_lost',
+          status: 'Request ILL',
+          requestability: 'yes'
+        }
+      end
+    end
+    trait :requested do
+      service_data do
+        {
+          status_code: 'requested',
+          status: 'Requested',
+          requestability: 'yes'
+        }
+      end
+    end
+    trait :offsite do
+      service_data do
+        {
+          status_code: 'offsite',
+          status: 'Offsite Available',
+          requestability: 'yes'
+        }
+      end
+    end
+    trait :processing do
+      service_data do
+        {
+          status_code: 'processing',
+          status: 'In Processing',
+          requestability: 'yes'
+        }
+      end
+    end
     factory :ill_service_response, traits: [:primo_source, :ill]
+    factory :billed_as_lost_service_response, traits: [:primo_source, :billed_as_lost]
+    factory :requested_service_response, traits: [:primo_source, :requested]
+    factory :on_order_service_response, traits: [:primo_source, :on_order]
+    factory :checked_out_service_response, traits: [:primo_source, :checked_out]
+    factory :available_service_response, traits: [:primo_source, :available]
+    factory :offsite_service_response, traits: [:primo_source, :offsite]
+    factory :processing_service_response, traits: [:primo_source, :processing]
   end
 end
