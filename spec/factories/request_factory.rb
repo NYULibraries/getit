87bd9bb447c924948f75fd848c4d591b42bf7f85
Scot Dalton <scotdalton@gmail.com>
2014-04-10 15:45:16 -0400

    Updated request/service response factories with necessary data

--- spec/factories/request_factory.rb
+++ spec/factories/request_factory.rb
@@ -1,3 +1,6 @@
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Request factory
 FactoryGirl.define do
   PRIMO_REFERRER = 'info:sid/primo.exlibrisgroup.com:primo-'
