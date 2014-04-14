02b39db9a9c84f9516378851025e2f5633247e3f
Scot Dalton <scotdalton@gmail.com>
2014-04-14 16:51:03 -0400

    Increase wait time so we get successes on our request button checks and don't wait for the holding, let Capybara handle the waiting

--- features/support/local_env.rb
+++ features/support/local_env.rb
@@ -24,6 +24,7 @@ require 'capybara/poltergeist'
 Capybara.configure do |config|
   # config.app_host = 'https://dev.login.library.nyu.edu'
   config.default_driver = :poltergeist
+  Capybara.default_wait_time = 20
 end
 
 VCR.configure do |config|
