9a79bb22bbb9e38fbc31afdb9a59646da8d680c5
Scot Dalton <scotdalton@gmail.com>
2014-04-14 16:41:25 -0400

    Fully implement the HoldingRequest policy for available, ill, recallable, processing, on order and offsite

--- spec/policies/holding_request_policy_spec.rb
+++ spec/policies/holding_request_policy_spec.rb
@@ -7,7 +7,7 @@ module Policies
   describe HoldingRequestPolicy do
     let(:service_response) { build(:service_response) }
     let(:holding) { Holding.new(service_response) }
-    let(:user) { build(:user) }
+    let(:user) { build(:aleph_user) }
     subject(:policy) do
       HoldingRequestPolicy.new(holding, user)
     end
@@ -22,12 +22,60 @@ module Policies
           it { should be_true }
         end
         context 'and the user is not nil' do
-          let(:user) { build(:user) }
+          let(:user) { build(:aleph_user) }
           it { should be_true }
         end
       end
-      context 'when the holding is in other states' do
-        it 'should respond appropriately'
+      context 'when the holding is never requestable' do
+        let(:service_response) { build(:never_requestable_service_response) }
+        context 'and the user is nil' do
+          let(:user) { nil }
+          it { should be_false }
+        end
+        context 'and the user is not nil' do
+          let(:user) { build(:aleph_user) }
+          it { should be_false }
+        end
+      end
+      context 'when the holding is always requestable' do
+        let(:service_response) { build(:always_requestable_service_response) }
+        context 'and the user is nil' do
+          let(:user) { nil }
+          it { should be_true }
+        end
+        context 'and the user is not nil' do
+          let(:user) { build(:aleph_user) }
+          context 'but the user does not have rights to request holdings in this sublibrary' do
+            before { user.user_attributes[:aleph_permissions]['BOBST'][:hold_permission] = 'N' }
+            context 'and the holding is not in an ILL state' do
+              before { service_response.service_data[:status_code] = 'available' }
+              it { should be_false }
+            end
+            context 'and the holding is in an ILL state' do
+              it { should be_true }
+            end
+          end
+          context 'and the user does have rights to request holdings in this sublibrary' do
+            it { should be_true }
+          end
+        end
+      end
+      context 'when the holding is requestable depending on the user requesting' do
+        let(:service_response) { build(:deferred_requestability_service_response) }
+        context 'and the user is nil' do
+          let(:user) { nil }
+          it { should be_false }
+        end
+        context 'and the user is not nil' do
+          let(:user) { build(:aleph_user) }
+          context 'but the user does not have rights to request holdings in this sublibrary' do
+            before { user.user_attributes[:aleph_permissions]['BOBST'][:hold_permission] = 'N' }
+            it { should be_false }
+          end
+          context 'and the user does have rights to request holdings in this sublibrary' do
+            it { should be_true }
+          end
+        end
       end
     end
     describe '#available?' do
@@ -39,25 +87,121 @@ module Policies
           it { should be_false }
         end
         context 'and the user is not nil' do
+          let(:user) { build(:aleph_user) }
           context 'but the user does not have rights to request holdings in this sublibrary' do
-            let(:user) { build(:user) }
+            before { user.user_attributes[:aleph_permissions]['BOBST'][:hold_permission] = 'N' }
             it { should be_false }
           end
           context 'and the user does have rights to request holdings in this sublibrary' do
             context 'but the user does not have rights to request holdings that are "available"' do
-              let(:user) { build(:user) }
+              before { user.user_attributes[:aleph_permissions]['BOBST'][:hold_on_shelf] = 'N' }
               it { should be_false }
             end
             context 'and the user has rights to request holdings that are "available"' do
-              let(:user) { build(:user) }
               it { should be_true }
             end
           end
         end
       end
-      context 'when the holding is in non "available" states' do
-        it 'should be false'
-        # it { should be_false }
+      context 'when the holding is in a non "available" state' do
+        let(:service_response) { build(:ill_service_response) }
+        it { should be_false }
+      end
+    end
+    describe '#recallable?' do
+      subject { policy.recallable? }
+      context 'when the holding is in a "recallable" state' do
+        let(:service_response) { build(:checked_out_service_response) }
+        context 'and the user is nil' do
+          let(:user) { nil }
+          it { should be_true }
+        end
+        context 'and the user is not nil' do
+          let(:user) { build(:aleph_user) }
+          context 'but the user does not have rights to request holdings in this sublibrary' do
+            before { user.user_attributes[:aleph_permissions]['BOBST'][:hold_permission] = 'N' }
+            it { should be_false }
+          end
+          context 'and the user does have rights to request holdings in this sublibrary' do
+            it { should be_true }
+          end
+        end
+      end
+      context 'when the holding is in a non "recallable" state' do
+        let(:service_response) { build(:ill_service_response) }
+        it { should be_false }
+      end
+    end
+    describe '#offsite?' do
+      subject { policy.offsite? }
+      context 'when the holding is in an "offsite" state' do
+        let(:service_response) { build(:offsite_service_response) }
+        context 'and the user is nil' do
+          let(:user) { nil }
+          it { should be_true }
+        end
+        context 'and the user is not nil' do
+          let(:user) { build(:aleph_user) }
+          context 'but the user does not have rights to request holdings in this sublibrary' do
+            before { user.user_attributes[:aleph_permissions]['BOBST'][:hold_permission] = 'N' }
+            it { should be_false }
+          end
+          context 'and the user does have rights to request holdings in this sublibrary' do
+            it { should be_true }
+          end
+        end
+      end
+      context 'when the holding is in a non "offsite" state' do
+        let(:service_response) { build(:ill_service_response) }
+        it { should be_false }
+      end
+    end
+    describe '#processing?' do
+      subject { policy.processing? }
+      context 'when the holding is in a "processing" state' do
+        let(:service_response) { build(:processing_service_response) }
+        context 'and the user is nil' do
+          let(:user) { nil }
+          it { should be_true }
+        end
+        context 'and the user is not nil' do
+          let(:user) { build(:aleph_user) }
+          context 'but the user does not have rights to request holdings in this sublibrary' do
+            before { user.user_attributes[:aleph_permissions]['BOBST'][:hold_permission] = 'N' }
+            it { should be_false }
+          end
+          context 'and the user does have rights to request holdings in this sublibrary' do
+            it { should be_true }
+          end
+        end
+      end
+      context 'when the holding is in a non "processing" state' do
+        let(:service_response) { build(:ill_service_response) }
+        it { should be_false }
+      end
+    end
+    describe '#on_order?' do
+      subject { policy.on_order? }
+      context 'when the holding is in a "on order" state' do
+        let(:service_response) { build(:on_order_service_response) }
+        context 'and the user is nil' do
+          let(:user) { nil }
+          it { should be_true }
+        end
+        context 'and the user is not nil' do
+          let(:user) { build(:aleph_user) }
+          context 'but the user does not have rights to request holdings in this sublibrary' do
+            before { user.user_attributes[:aleph_permissions]['BOBST'][:hold_permission] = 'N' }
+            it { should be_false }
+          end
+          context 'and the user does have rights to request holdings in this sublibrary' do
+            it { should be_true }
+          end
+        end
+      end
+      context 'when the holding is in a non "on order" state' do
+        let(:service_response) { build(:ill_service_response) }
+        it { should be_false }
       end
     end
     describe '#ill?' do
@@ -69,7 +213,7 @@ module Policies
           it { should be_true }
         end
         context 'and the user is not nil' do
-          let(:user) { build(:user) }
+          let(:user) { build(:aleph_user) }
           it { should be_true }
         end
       end
@@ -80,12 +224,9 @@ module Policies
           it { should be_false }
         end
         context 'and the user is not nil' do
-          let(:user) { build(:user) }
+          let(:user) { build(:aleph_user) }
           it { should be_false }
         end
-      end
-      context 'when the holding is in other states' do
-        it 'should respond appropriately'
       end
     end
     context 'when initialized without any arguments' do
