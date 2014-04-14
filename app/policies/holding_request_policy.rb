5cb5cdf015b4cbf76bffb836bcf1bcd9776ce023
Scot Dalton <scotdalton@gmail.com>
2014-04-14 16:41:25 -0400

    Fully implement the HoldingRequest policy for available, ill, recallable, processing, on order and offsite

--- app/policies/holding_request_policy.rb
+++ app/policies/holding_request_policy.rb
@@ -4,8 +4,12 @@ module Policies
       super
     end
 
+    # Status is requestable if the Holding is in an 
+    # ILL state or the combination of User and Holding
+    # is requestable
     def requestable?
-      ill? && available?
+      (ill? || available? || recallable? ||
+        processing? || on_order? || offsite?)
     end
 
     def ill?
@@ -13,12 +17,46 @@ module Policies
     end
 
     def available?
-      holding.available? && aleph_user.can_request_available?
+      combo_requestable? && holding.available? && user_can_request_available?
+    end
+
+    def recallable?
+      combo_requestable? && holding.recallable?
+    end
+    alias_method :recall?, :recallable?
+
+    def offsite?
+      combo_requestable? && holding.offsite?
+    end
+
+    def processing?
+      combo_requestable? && holding.processing?
+    end
+    alias_method :in_processing?, :processing?
+
+    def on_order?
+      combo_requestable? && holding.on_order?
     end
 
     private
+    def combo_requestable?
+      always_requestable? || (holding.requestable? && user_can_request?)
+    end
+
+    def always_requestable?
+      holding.always_requestable? && aleph_user.nil?
+    end
+
+    def user_can_request?
+      aleph_user.present? && aleph_user.can_request?(holding)
+    end
+
+    def user_can_request_available?
+      aleph_user.present? && aleph_user.can_request_available?(holding)
+    end
+
     def aleph_user
-      @aleph_user ||= AlephUser.new(user)
+      @aleph_user ||= AlephUser.new(user) unless user.nil?
     end
   end
 end
