4ff1c52067440eed70bf6fcef953e4582322213a
Scot Dalton <scotdalton@gmail.com>
2014-07-02 11:11:03 -0400

    Move UserSession tests to specs and remove unnecessary Aleph permissions call back for :additional_attributes since this functionality is now handled by the Exlibris::Aleph::Patron::Record::Item

---
+++ spec/models/user_session_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+describe UserSession do
+  let(:user) { build(:user) }
+  describe '.pds_url' do
+    subject { UserSession.pds_url }
+    it { should eq ENV['PDS_URL'] }
+  end
+  describe '.redirect_logout_url' do
+    subject { UserSession.redirect_logout_url }
+    it { should eq 'http://bobcat.library.nyu.edu' }
+  end
+  describe '.calling_system' do
+    subject { UserSession.calling_system }
+    it { should eq 'getit' }
+  end
+  describe '.institution_param_key' do
+    subject { UserSession.institution_param_key }
+    it { should eq 'umlaut.institution' }
+  end
+end
