c721f0897bc95bf9f37ef9ecb55ff198a9643f20
Scot Dalton <scotdalton@gmail.com>
2014-07-02 12:03:36 -0400

    Update unit tests in preparation to move to specs

--- test/unit/sfx4_solr_search_ns_test.rb
+++ test/unit/sfx4_solr_search_ns_test.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require 'test_helper'
 class Sfx4SolrSearchNsTest < ActiveSupport::TestCase
   include SearchMethods::Sfx4Solr::Ns
@@ -6,43 +7,37 @@ class Sfx4SolrSearchNsTest < ActiveSupport::TestCase
       assert_equal Sfx4::Ns::Base, SearchMethods::Sfx4Solr::Ns.sfx4_base
     end
   end
-  
+
   test "fetch urls? NS" do
     if Sfx4::Ns::Base.connection_configured?
       assert SearchMethods::Sfx4Solr::Ns.fetch_urls?
     end
   end
-  
+
   test "sfx4solr title contains search NS" do
-    # if Sfx4::Ns::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr title contains search NS') do
-        query = "economist"
-        search_type = "contains"
-        first_result = "De Economist"
-        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
-      end
-    # end
+    VCR.use_cassette('sfx4solr title contains search NS') do
+      query = "economist"
+      search_type = "contains"
+      first_result = "De Economist"
+      assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
+    end
   end
-  
+
   test "sfx4solr title starts with search NS" do
-    # if Sfx4::Ns::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr title starts with search NS') do
-        query = "Journal of"
-        search_type = "begins"
-        first_result = "Journal of accountancy"
-        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
-      end
-    # end
+    VCR.use_cassette('sfx4solr title starts with search NS') do
+      query = "Journal of"
+      search_type = "begins"
+      first_result = "Journal of Anthropology"
+      assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
+    end
   end
-  
+
   test "sfx4solr title exact search NS" do
-    # if Sfx4::Ns::AzTitle.connection_configured?
-      VCR.use_cassette('sfx4solr title exact search NS') do
-        query = "The New Yorker"
-        search_type = "exact"
-        first_result = "The New - Yorker (1836-1841)"
-        assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
-      end
-    # end
+    VCR.use_cassette('sfx4solr title exact search NS') do
+      query = 'The New Yorker'
+      search_type = 'exact'
+      first_result = 'The New - Yorker (1836-1841)'
+      assert_equal first_result, _search_by_title(query, search_type).hits.first.stored(:title_display)
+    end
   end
 end
