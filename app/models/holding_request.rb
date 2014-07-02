09253addd61264ec4c6bbf33a7b831aba8fdde1c
Scot Dalton <scotdalton@gmail.com>
2014-07-02 10:38:52 -0400

    Add the HoldingRequest model to manage requests on GetIt::Holdings

---
+++ app/models/holding_request.rb
@@ -0,0 +1,42 @@
+class HoldingRequest
+  attr_reader :holding, :user
+  def initialize(holding, user)
+    unless(holding.is_a?(GetIt::Holding::NyuAleph))
+      raise ArgumentError.new("Expecting #{holding} to be a GetIt::Holding::NyuAleph")
+    end
+    unless(user.is_a?(User))
+      raise ArgumentError.new("Expecting #{user} to be a User")
+    end
+    @holding = holding
+    @user = user
+  end
+
+  def circulation_policy
+    @circulation_policy ||= item.circulation_policy
+  end
+
+  def create_hold(parameters)
+    item.create_hold(parameters)
+  end
+
+  private
+  def item
+    @item ||= record.item(item_id)
+  end
+
+  def record
+    @record ||= aleph_patron.record(record_id)
+  end
+
+  def record_id
+    @record_id ||= holding.record_id
+  end
+
+  def item_id
+    @item_id ||= holding.item_id
+  end
+
+  def aleph_patron
+    @aleph_patron ||= GetIt::AlephPatron.new(user)
+  end
+end
