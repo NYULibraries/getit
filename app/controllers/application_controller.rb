d8100cf92a04486c8c98cb10e01ba9dcb7cd99d8
Scot Dalton <scotdalton@gmail.com>
2014-07-02 11:33:50 -0400

    Add ApplicationController spec for #url_for_request and use the seed username 'dev123' instead of reading from setting for clarity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include Authpds::Controllers::AuthpdsController
+
   # For dev purposes
   def current_user_dev
-   @current_user_dev ||= User.find_by_username(Settings.seeds.user.username)
+   @current_user_dev ||= User.find_by_username('dev123')
   end
   alias :current_user :current_user_dev if Rails.env.development?
 
