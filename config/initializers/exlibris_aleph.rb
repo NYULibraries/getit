085bfdc259c43f1547ce296cbd7a971f734407a7
Scot Dalton <scotdalton@gmail.com>
2014-07-02 11:58:47 -0400

    Load up the Exlibris::Aleph::TablesManager when the application starts to save time

---
+++ config/initializers/exlibris_aleph.rb
@@ -0,0 +1,4 @@
+# Load all the sub libraries when the application starts
+Exlibris::Aleph::TablesManager.instance.sub_libraries.all
+# Load all the item circulation policies when the application starts
+Exlibris::Aleph::TablesManager.instance.item_circulation_policies.all
