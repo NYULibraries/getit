7445370d759ffe1b2de644540fc1efd918771a49
Scot Dalton <scotdalton@gmail.com>
2014-07-02 12:08:13 -0400

    Update user sessions controller tests in prep to move to specs

---
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -0,0 +1,3 @@
+require 'rails_helper'
+describe UserSessionsController, vcr: {cassette_name: 'user sessions'}  do
+end
--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -4,6 +4,6 @@ class UserSessionsControllerTest < ActionController::TestCase
   test "should redirect to login page" do
     get :new
     assert_response :redirect
-    assert_redirected_to "#{Settings.pds.login_url}/pds?func=load-login&institute=NYU&calling_system=getit&url=http%3A%2F%2Ftest.host%2Fvalidate%3Freturn_url%3Dhttp%253A%252F%252Ftest.host%252F%26getit_controller%3Duser_sessions%26getit_action%3Dnew", "Redirected to incorrect URL."
+    assert_redirected_to "#{ENV['PDS_URL']}/pds?func=load-login&institute=NYU&calling_system=getit&url=http%3A%2F%2Ftest.host%2Fvalidate%3Freturn_url%3Dhttp%253A%252F%252Ftest.host%252F%26getit_controller%3Duser_sessions%26getit_action%3Dnew", "Redirected to incorrect URL."
   end
 end
