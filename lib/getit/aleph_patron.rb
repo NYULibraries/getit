abcc96d3b65e280509caae21363bec40b289b7fc
Scot Dalton <scotdalton@gmail.com>
2014-07-02 10:37:37 -0400

    Add an AlephPatron class to decorate a User with Exlibris::Aleph::Patron functionality

---
+++ lib/getit/aleph_patron.rb
@@ -0,0 +1,25 @@
+module GetIt
+  class AlephPatron
+    attr_reader :user, :id
+
+    def initialize(user)
+      unless user.is_a?(User)
+        raise ArgumentError.new("Expecting #{user} to be a User")
+      end
+      unless user.user_attributes.is_a?(Hash)
+        raise ArgumentError.new("Expecting #{user.user_attributes} to be a Hash")
+      end
+      @user = user
+      @id = user.user_attributes[:nyuidn]
+    end
+
+    def record(record_id)
+      patron.record(record_id)
+    end
+
+    private
+    def patron
+      @patron ||= Exlibris::Aleph::Patron.new(id)
+    end
+  end
+end
---
+++ spec/getit/aleph_patron_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+module GetIt
+  describe AlephPatron do
+    let(:user) { build :aleph_user }
+    subject(:aleph_patron) { AlephPatron.new(user) }
+    describe '#user' do
+      subject { aleph_patron.user }
+      it { should eq user }
+    end
+    describe '#id' do
+      subject { aleph_patron.id }
+      it { should eq (ENV['BOR_ID'] || 'BOR_ID') }
+    end
+    describe '#record', vcr: {cassette_name: 'patron'} do
+      let(:record_id) { 'NYU01000980206' }
+      subject { aleph_patron.record(record_id) }
+      it { should be_an Exlibris::Aleph::Patron::Record }
+    end
+    context 'when initialized without any arguments' do
+      it 'should raise an ArgumentError' do
+        expect { AlephPatron.new }.to raise_error ArgumentError
+      end
+    end
+    context 'when initialized with a user argument' do
+      context 'but the user argument is not a User' do
+        let(:user) { "invalid" }
+        it 'should raise an ArgumentError' do
+          expect { subject }.to raise_error ArgumentError
+        end
+      end
+      context 'and the user argument is a User' do
+        context 'but the user attributes are not a Hash' do
+          let(:user) { build :user, user_attributes: nil }
+          it 'should raise an ArgumentError' do
+            expect { subject }.to raise_error ArgumentError
+          end
+        end
+      end
+    end
+  end
+end
