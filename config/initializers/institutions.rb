c180a18b253c740dafcd50ff75d21330f9436687
Scot Dalton <scotdalton@gmail.com>
2014-07-07 13:54:05 -0400

    Move institutions.yml to ip_addresses.yml and add the file to the Institutions load path when the 'INSTITUTIONS' environment variable is present

--- config/initializers/institutions.rb
+++ config/initializers/institutions.rb
@@ -1,1 +1,2 @@
-Institutions.filenames << "umlaut_services.yml"
+Institutions.filenames << 'umlaut_services.yml'
+Institutions.filenames << 'ip_addresses' if ENV.has_key?('INSTITUTIONS')
--- test/unit/institutions_test.rb
+++ test/unit/institutions_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 class InstitutionsTest < ActiveSupport::TestCase
   test "ip addresses" do
-    ips =  Institutions.institutions[:NYU].ip_addresses
-    assert_not_nil ips
+    ips = Institutions.institutions[:NYU].ip_addresses
+    assert_not_nil(ips) if ENV.has_key?('INSTITUTIONS')
   end
 end
