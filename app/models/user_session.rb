2d430095f610e26d4f6787b29ced061021cf595c
Scot Dalton <scotdalton@gmail.com>
2014-07-07 13:52:52 -0400

    Default PDS URL to login.library.nyu.edu

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,5 +1,5 @@
 class UserSession < Authlogic::Session::Base
-  pds_url ENV['PDS_URL']
+  pds_url (ENV['PDS_URL'] || 'https://login.library.nyu.edu')
   redirect_logout_url 'http://bobcat.library.nyu.edu'
   calling_system 'getit'
   institution_param_key 'umlaut.institution'
