22c99f47b80ff035edcd53503bbd37e38ea9c876
Scot Dalton <scotdalton@gmail.com>
2014-07-07 13:52:52 -0400

    Default PDS URL to login.library.nyu.edu

--- spec/models/user_session_spec.rb
+++ spec/models/user_session_spec.rb
@@ -3,7 +3,7 @@ describe UserSession do
   let(:user) { build(:user) }
   describe '.pds_url' do
     subject { UserSession.pds_url }
-    it { should eq ENV['PDS_URL'] }
+    it { should eq (ENV['PDS_URL'] || 'https://login.library.nyu.edu') }
   end
   describe '.redirect_logout_url' do
     subject { UserSession.redirect_logout_url }
