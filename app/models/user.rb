05638371e16eaccbcbdfb2c0517cbb2907337721
Scot Dalton <scotdalton@gmail.com>
2014-07-07 14:18:28 -0400

    Remove unused code from the user model; this functionality is now encapsulated in the HoldingRequest; closes #36

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,21 +12,4 @@ class User < ActiveRecord::Base
     c.require_password_confirmation = false
     c.disable_perishable_token_maintenance = true
   end
-
-  # Create a hold in Aleph on the given holding for user
-  def create_hold(holding, hold_options)
-    patron.place_hold(holding.adm_library_code, holding.original_source_id, 
-      holding.source_record_id, holding.item_id, hold_options)
-  end
-
-  # Return the error from the Aleph patron.
-  def error
-    patron.error
-  end
-
-  # Aleph Patron for placing holds
-  def patron
-    @patron ||= Exlibris::Aleph::Patron.new(patron_id: user_attributes[:nyuidn])
-  end
-  private :patron
 end
