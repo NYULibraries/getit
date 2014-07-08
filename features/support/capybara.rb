22f990ac427fb42cbc82897dd79e0b10e5713591
Scot Dalton <scotdalton@gmail.com>
2014-07-08 10:57:22 -0400

    Increase Capybara default wait time from 15 to 20 seconds

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -4,5 +4,5 @@ Capybara.configure do |config|
   # config.app_host = 'https://dev.login.library.nyu.edu'
   config.default_driver = :poltergeist
   # config.default_driver = :selenium
-  Capybara.default_wait_time = 15
+  Capybara.default_wait_time = 20
 end
