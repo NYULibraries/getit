6b30df3658e8d581a19f830216a0af774c53e2cd
Scot Dalton <scotdalton@gmail.com>
2014-07-08 09:28:29 -0400

    Print the exlibris-aleph initializer error message to standard output

--- config/initializers/exlibris_aleph.rb
+++ config/initializers/exlibris_aleph.rb
@@ -4,5 +4,7 @@ begin
   # Load all the item circulation policies when the application starts
   Exlibris::Aleph::TablesManager.instance.item_circulation_policies.all
 rescue Errno::ENOENT => e
-  Rails.logger.warn("Skipping Exlibris::Aleph initialization since \"#{e.message}\"")
+  message = "Skipping Exlibris::Aleph initialization since \"#{e.message}\""
+  p message
+  Rails.logger.warn(message)
 end
