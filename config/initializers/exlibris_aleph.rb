4fde9cf738f6727c355f381c8fb2aff31f9abf5d
Scot Dalton <scotdalton@gmail.com>
2014-07-08 10:11:34 -0400

    Print a message to standard output indicating that Exlibris::Aleph has completed initialization

--- config/initializers/exlibris_aleph.rb
+++ config/initializers/exlibris_aleph.rb
@@ -3,6 +3,7 @@ begin
   Exlibris::Aleph::TablesManager.instance.sub_libraries.all
   # Load all the item circulation policies when the application starts
   Exlibris::Aleph::TablesManager.instance.item_circulation_policies.all
+  p 'Exlibris::Aleph initialized'
 rescue Errno::ENOENT => e
   message = "Skipping Exlibris::Aleph initialization since \"#{e.message}\""
   p message
