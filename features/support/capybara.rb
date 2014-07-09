829b5fbd4485cfc50bbce7cd1983ddbf68d9d5e3
Scot Dalton <scotdalton@gmail.com>
2014-07-09 13:14:40 -0400

    Uncomment the Capybar default wait time and set it obnoxiously high

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,11 +1,11 @@
 # Configure Capybara
 require 'capybara/poltergeist'
 Capybara.configure do |config|
-  # config.default_driver = :selenium
-  # Capybara.default_wait_time = 120
   Capybara.register_driver :poltergeist do |app|
     Capybara::Poltergeist::Driver.new(app, timeout: 120, js_errors: true, phantomjs_options: ['--load-images=no'])
   end
+  # config.default_driver = :selenium
+  Capybara.default_wait_time = 120
   config.default_driver = :poltergeist
   config.javascript_driver = :poltergeist
 end
