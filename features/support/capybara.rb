3712dae7db2e89a091a1f00c06b4e48319b7ab0f
Scot Dalton <scotdalton@gmail.com>
2014-07-09 11:15:04 -0400

    Add Poltergeist debugging options

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -2,7 +2,7 @@
 require 'capybara/poltergeist'
 Capybara.configure do |config|
   Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, timeout: 120)
+    Capybara::Poltergeist::Driver.new(app, timeout: 120, debug: true, js_errors: true, phantomjs_options: ['--load-images=no'])
   end
   # config.default_driver = :selenium
   config.default_driver = :poltergeist
