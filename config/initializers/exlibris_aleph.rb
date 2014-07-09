3efcd26d9e11279063cbf8117d7e70089cfc0926
Scot Dalton <scotdalton@gmail.com>
2014-07-09 10:46:25 -0400

    Set Exlibris::Aleph collections at initialization

--- config/initializers/exlibris_aleph.rb
+++ config/initializers/exlibris_aleph.rb
@@ -1,6 +1,8 @@
 begin
   # Load all the sub libraries when the application starts
   Exlibris::Aleph::TablesManager.instance.sub_libraries.all
+  # Load all the collections when the application starts
+  Exlibris::Aleph::TablesManager.instance.collections.all
   # Load all the item circulation policies when the application starts
   Exlibris::Aleph::TablesManager.instance.item_circulation_policies.all
 rescue Errno::ENOENT => e
