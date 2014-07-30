6980f36ad50ffb8fe06f75c897dad40cc4c67e77
Scot Dalton <scotdalton@gmail.com>
2014-07-30 08:59:11 -0400

    Clean up a bit of whitespace in the request button step definitions file

--- features/step_definitions/request_button_steps.rb
+++ features/step_definitions/request_button_steps.rb
@@ -10,7 +10,6 @@ Then(/^I should see a link to login for request options$/) do
   expect(request_login_link[:href]).to match /^http:\/\/[^\/]+\/login/
 end
 
-
 Then(/^I should not see a link to login for request options$/) do
   expect(page).not_to have_css(request_login_link_css, text: 'Login for Request Options')
 end
@@ -18,7 +17,6 @@ end
 Then(/^I should see a request button$/) do
   expect(page).to have_css(request_button_css, text: 'Request')
 end
-
 
 Then(/^I should not see a request button$/) do
   expect(page).not_to have_css(request_button_css, text: 'Request')
