01bd9d8b9752afeb57646ddb0c7e6b5b29cfdc1f
Scot Dalton <scotdalton@gmail.com>
2014-10-02 14:10:12 -0400

    Use the new columns and remove the old ones in the UserFactory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,29 +1,16 @@
 # User factory
 FactoryGirl.define do
   factory :user do
-    sequence(:username) { |n| "developer#{n}"}
-    email 'developer@example.com'
+    sequence :username do |n| "user#{n}" end
+    provider 'nyu_shibboleth'
+    email { "#{username}@example.com" }
     firstname 'Dev'
     lastname 'Eloper'
+    institution_code 'NYU'
+    aleph_id { (ENV['BOR_ID'] || 'BOR_ID') }
     trait :nyu_aleph_attributes do
-      user_attributes do
-        {
-          nyuidn: (ENV['BOR_ID'] || 'BOR_ID'),
-          primary_institution: :NYU,
-          institutions: [:NYU],
-          bor_status: '51'
-        }
-      end
     end
     trait :cooper_union_aleph_attributes do
-      user_attributes do
-        {
-          nyuidn: (ENV['BOR_ID'] || 'BOR_ID'),
-          primary_institution: :CU,
-          institutions: [:CU],
-          bor_status: '10'
-        }
-      end
     end
     factory :aleph_user, traits: [:nyu_aleph_attributes]
     factory :ezborrow_user, traits: [:nyu_aleph_attributes]
