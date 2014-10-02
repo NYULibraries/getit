9423b39d9fefc5b48f0ff44e8f53899458f737ae
Scot Dalton <scotdalton@gmail.com>
2014-10-02 13:51:38 -0400

    Update the User model to use Devise
    - User model is omniauthable and trackable
    - Providers must be in the expected list
    - Add examples to the User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 describe User do
+  describe User::VALID_PROVIDERS do
+    subject { User::VALID_PROVIDERS }
+    it { should include 'nyu_shibboleth' }
+    it { should include 'new_school_ldap' }
+    it { should include 'aleph' }
+    it { should include 'twitter' }
+    it { should include 'facebook' }
+  end
+
   subject(:user) { build(:user) }
   it { should be_a User }
+
+  context 'when the provider code is given' do
+    subject(:user) { build(:user, provider: provider) }
+    context 'and it is invalid' do
+      let(:provider) { 'invalid' }
+      it { should_not be_valid }
+    end
+    context 'and it is nil' do
+      let(:provider) { nil }
+      it { should_not be_valid }
+    end
+    context 'and it is empty' do
+      let(:provider) { '' }
+      it { should_not be_valid }
+    end
+    context 'and it is valid' do
+      let(:provider) { 'aleph' }
+      it { should be_valid }
+    end
+  end
+
 end
