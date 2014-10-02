66272b83da9e0e696aa59cb688341914e8be5f58
Scot Dalton <scotdalton@gmail.com>
2014-10-02 13:55:49 -0400

    Add validation for institution codes and make institution code and aleph id "attr_accessible"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   VALID_PROVIDERS = %w{ nyu_shibboleth new_school_ldap aleph twitter facebook }
+  VALID_INSTITUTION_CODES = Institutions.institutions.keys.map(&:to_s)
 
   # Available devise modules are:
   # :database_authenticatable, :registerable,
@@ -10,8 +11,19 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :provider, :email, :firstname, :lastname,
+    :institution_code, :aleph_id
 
   # Must have a valid provider
   validates :provider, inclusion: { in: VALID_PROVIDERS }
 
+  # Must have a valid institution code
+  validates :institution_code, inclusion: {in: VALID_INSTITUTION_CODES},
+    allow_blank: true
+
+  # Resolve the institution based on the institution_code
+  def institution
+    unless institution_code.blank?
+      @institution ||= Institutions.institutions[institution_code.to_sym]
+    end
+  end
 end
