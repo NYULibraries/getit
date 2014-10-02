152cd8430ccd2e6c276dfa0d1b762109f6ca0545
Scot Dalton <scotdalton@gmail.com>
2014-10-02 13:51:38 -0400

    Update the User model to use Devise
    - User model is omniauthable and trackable
    - Providers must be in the expected list
    - Add examples to the User spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  VALID_PROVIDERS = %w{ nyu_shibboleth new_school_ldap aleph twitter facebook }
+
+  # Available devise modules are:
+  # :database_authenticatable, :registerable,
+  # :recoverable, :rememberable, :trackable, :validatable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :omniauthable, :trackable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :username, :provider, :email, :firstname, :lastname,
+
+  # Must have a valid provider
+  validates :provider, inclusion: { in: VALID_PROVIDERS }
+
 end
