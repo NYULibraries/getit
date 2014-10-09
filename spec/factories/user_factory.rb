d923859dc7eb713d75c7bed04d7d03ad0946297c
Scot Dalton <scotdalton@gmail.com>
2014-10-09 15:29:27 -0400

    Add user factories with both E-ZBorrow and non E-ZBorrow statuses to the User factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
     password_salt Authlogic::Random.hex_token
     crypted_password { Authlogic::CryptoProviders::Sha512.encrypt("#{username}#{password_salt}") }
     persistence_token Authlogic::Random.hex_token
-    trait :aleph_attributes do
+    trait :nyu_aleph_attributes do
       user_attributes do
         {
           nyuidn: (ENV['BOR_ID'] || 'BOR_ID'),
@@ -18,6 +18,18 @@ FactoryGirl.define do
         }
       end
     end
-    factory :aleph_user, traits: [:aleph_attributes]
+    trait :cooper_union_aleph_attributes do
+      user_attributes do
+        {
+          nyuidn: (ENV['BOR_ID'] || 'BOR_ID'),
+          primary_institution: :CU,
+          institutions: [:CU],
+          bor_status: '10'
+        }
+      end
+    end
+    factory :aleph_user, traits: [:nyu_aleph_attributes]
+    factory :ezborrow_user, traits: [:nyu_aleph_attributes]
+    factory :non_ezborrow_user, traits: [:cooper_union_aleph_attributes]
   end
 end
