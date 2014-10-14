5199faf349708f7ae1c21012362d1f579545bdfd
Scot Dalton <scotdalton@gmail.com>
2014-10-14 14:22:21 -0400

    Update NyuAleph Holdings to be created from COURSES Primo source ServiceResponses

--- lib/getit/holding/nyu_aleph.rb
+++ lib/getit/holding/nyu_aleph.rb
@@ -1,7 +1,7 @@
 module GetIt
   module Holding
     class NyuAleph < GetIt::Holding::PrimoSource
-      VALID_SOURCES = ['nyu_aleph']
+      VALID_SOURCES = ['nyu_aleph', 'COURSES']
       ILL_STATUSES = ['Request ILL', 'On Order']
 
       attr_reader :record_id, :item_id, :institution
