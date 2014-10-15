af24287f0a1c314a4dfee4b7f21e3ecdd672edbd
Scot Dalton <scotdalton@gmail.com>
2014-10-15 14:10:11 -0400

    Add the StackMapPolicy object for determining "mappability"

--- app/policies/stackmap_policy.rb
+++ app/policies/stackmap_policy.rb
@@ -1,4 +1,11 @@
 class StackMapPolicy
+  MAPPABLE_SUB_LIBRARY_CODES = %w[TNSGI]
+  MAPPABLE_COLLECTION_CODES = %w[MAIN OVERZ]
+  MAPPABLE_LOCATIONS = [
+    "New School University Center Main Collection",
+    "New School University Center Oversize Collection"
+  ]
+
   attr_reader :holding
 
   def initialize(holding)
@@ -6,5 +13,24 @@ class StackMapPolicy
       raise ArgumentError.new("Expecting #{holding} to be a GetIt::Holding::Base")
     end
     @holding = holding
+  end
+
+  def mappable?
+    if holding.is_a?(GetIt::Holding::NyuAleph)
+      holding.available? &&
+        MAPPABLE_SUB_LIBRARY_CODES.include?(sub_library.code) &&
+          MAPPABLE_COLLECTION_CODES.include?(collection.code)
+    else
+      MAPPABLE_LOCATIONS.include?(holding.location)
+    end
+  end
+
+  private
+  def sub_library
+    @sub_library ||= holding.sub_library
+  end
+
+  def collection
+    @collection ||= holding.collection
   end
 end
--- spec/policies/stackmap_policy_spec.rb
+++ spec/policies/stackmap_policy_spec.rb
@@ -1,11 +1,78 @@
 require 'rails_helper'
 describe StackMapPolicy do
   let(:service_response) { build(:nyu_aleph_service_response) }
-  let(:holding) { GetIt::Holding::NyuAleph.new(service_response) }
+  let(:holding) { GetIt::HoldingManager.new(service_response).holding }
   subject(:stackmap_policy) { StackMapPolicy.new(holding) }
   describe '#holding' do
     subject { stackmap_policy.holding }
     it { should eq holding }
+  end
+  describe '#mappable?' do
+    subject { stackmap_policy.mappable? }
+    context 'when the holding is a book' do
+      context 'and it is in the "New School Main Collection"' do
+        context 'and it is "available"' do
+          let(:service_response) do
+            build(:available_new_school_main_collection_service_response)
+          end
+          it { should be true }
+        end
+        context 'and it is "checked out"' do
+          let(:service_response) do
+            build(:checked_out_new_school_main_collection_service_response)
+          end
+          it { should be false }
+        end
+        context 'and it is "offsite"' do
+          let(:service_response) do
+            build(:offsite_new_school_main_collection_service_response)
+          end
+          it { should be false }
+        end
+      end
+      context 'and it is in the "New School Oversize Collection"' do
+        context 'and it is "available"' do
+          let(:service_response) do
+            build(:available_new_school_oversize_collection_service_response)
+          end
+          it { should be true }
+        end
+        context 'and it is "checked out"' do
+          let(:service_response) do
+            build(:checked_out_new_school_oversize_collection_service_response)
+          end
+          it { should be false }
+        end
+        context 'and it is "offsite"' do
+          let(:service_response) do
+            build(:offsite_new_school_oversize_collection_service_response)
+          end
+          it { should be false }
+        end
+      end
+      context "but it is not in the New School's Main or Oversize Collections" do
+        let(:service_response) { build(:nyu_aleph_service_response) }
+        it { should be false }
+      end
+    end
+    context 'when the holding is a journal' do
+      context 'and it is in the "New School Main Collection"' do
+        let(:service_response) do
+          build(:new_school_main_collection_journal_service_response)
+        end
+        it { should be true }
+      end
+      context 'and it is in the "New School Oversize Collection"' do
+        let(:service_response) do
+          build(:new_school_oversize_collection_journal_service_response)
+        end
+        it { should be true }
+      end
+      context "but it is not in the New School's Main or Oversize Collections" do
+        let(:service_response) { build(:primo_service_response) }
+        it { should be false }
+      end
+    end
   end
 
   context 'when initialized with a holding argument' do
