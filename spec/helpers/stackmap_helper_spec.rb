afaef05374636df3d7e20a13fab1ac61c8bff809
Scot Dalton <scotdalton@gmail.com>
2014-10-16 16:05:07 -0400

    Fix the StackMapHelper specs

--- spec/helpers/stackmap_helper_spec.rb
+++ spec/helpers/stackmap_helper_spec.rb
@@ -1,5 +1,7 @@
 require "rails_helper"
 describe EZBorrowHelper do
+  let(:service_response) { build(:nyu_aleph_service_response) }
+  let(:holding) { GetIt::HoldingManager.new(service_response).holding }
   describe '#stackmap_policy' do
     subject { helper.stackmap_policy(holding) }
     it { should be_a StackMapPolicy }
