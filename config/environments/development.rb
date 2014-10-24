0c177bb8ebcd56254621bd676854bd8da608f298
Scot Dalton <scotdalton@gmail.com>
2014-10-24 10:34:27 -0400

    Update the config environments for the new version of Rails and Umlaut

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,47 +1,47 @@
-GetIt::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
   #
-  # UMLAUT: Umlaut's use of threading is not compatible with class
-  # reloading, even in development. Umlaut requires true here.
-  # Rails 3.2 _might_ let you get away with false when it comes out.
-  config.cache_classes = true
+  # UMLAUT: Umlaut's use of threading makes Rails dev-mode class reloading tricky
+  # It seems to be be mostly okay with cache_classes=false AND eager_load=true
+  # but beware of editing files while background requests are running. 
+  config.eager_load = true
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
-  # Show full error reports and disable caching
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
+  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  # Print deprecation notices to the Rails logger
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
 
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-  config.active_record.logger = nil
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
   config.assets.debug = true
 
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+
+  # Use pry as the console in development
   console do
-    # this block is called only when running console,
+    # This block is called only when running console,
     # so we can safely require pry here
     require "pry"
     config.console = Pry
