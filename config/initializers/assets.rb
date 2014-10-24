5325c9476e1e89432455de653b9b401739e9b860
Scot Dalton <scotdalton@gmail.com>
2014-10-24 10:35:18 -0400

    Update the initializers for the new version of Rails

---
+++ config/initializers/assets.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+# Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+Rails.application.config.assets.precompile += %w[
+  search.js
+  resolve.js
+  search.css
+  search_cu.css
+  search_nyuad.css
+  search_ns.css
+  search_hsl.css
+  search_nyush.css
+  search_nysid.css
+  resolve.css
+  resolve_cu.css
+  resolve_nyuad.css
+  resolve_ns.css
+  resolve_hsl.css
+  resolve_nyush.css
+  resolve_nysid.css
+]
