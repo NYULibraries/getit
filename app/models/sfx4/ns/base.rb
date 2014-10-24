c87ea64743fcfd8c84802aba7d6e6158933bb869
Scot Dalton <scotdalton@gmail.com>
2014-10-24 11:12:34 -0400

    Remove whitelisting code from the models for Rails 4
    This functionality has been moved to the controllers in Rails 4 and these models don't have associated controllers so it's unnecessary.

--- app/models/sfx4/ns/base.rb
+++ app/models/sfx4/ns/base.rb
@@ -1,9 +1,6 @@
 module Sfx4
   module Ns
     class Base < ActiveRecord::Base
-      # This model is readonly.
-      # Don't whitelist any attributes
-      attr_accessible
 
       # Was a SFX DB connection set in database.yml to connect directly to sfx?
       def self.connection_configured?
@@ -14,13 +11,13 @@ module Sfx4
       self.establish_connection :sfx4_ns if self.connection_configured?
 
       # ActiveRecord likes it when we tell it this is an abstract
-      # class only. 
-      self.abstract_class = true 
+      # class only.
+      self.abstract_class = true
 
       extend Sfx4::Abstract::Base
 
       # All SFX things are read-only!
-      def readonly?() 
+      def readonly?()
         return true
       end
     end
