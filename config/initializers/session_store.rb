643f536085e8bd021d15218f5068e83a262d06ae
Scot Dalton <scotdalton@gmail.com>
2014-10-24 10:35:18 -0400

    Update the initializers for the new version of Rails

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.session_store ActionDispatch::Session::CacheStore,
-  key: '_getit_session', compress: true
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# GetIt::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_get_it_session'
