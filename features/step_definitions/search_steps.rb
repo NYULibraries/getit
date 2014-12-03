0d317a77b0c5a1c9adc190ca143ef05acbefdcb9
B Alter <barnaby.alter@gmail.com>
2014-12-03 17:26:36 -0500

    Trying to remove some hocuspocus from step definition to get this thing to pass on travis

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,6 +1,3 @@
 Given(/^I am on the GetIt search page$/) do
-  # When using poltergeist visiting a relative url
-  # doesn't seem to rack up so manually do it
-  visit '/' if poltergeist? # Rack up
   visit '/search'
 end
