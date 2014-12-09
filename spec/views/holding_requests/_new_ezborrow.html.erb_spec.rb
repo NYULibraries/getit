4d02149e2a3f68bba215b306b527261026b6a8c9
Barnaby <barnaby.alter@nyu.edu>
2014-12-09 16:49:41 -0500

    Updated rspec for new ezborrow and ill text

--- spec/views/holding_requests/_new_ezborrow.html.erb_spec.rb
+++ spec/views/holding_requests/_new_ezborrow.html.erb_spec.rb
@@ -11,6 +11,6 @@ describe 'holding_requests/_new_ezborrow', vcr: {cassette_name: 'holding request
   end
   it { should match /\/holding_requests\/#{_service_response.id}\/ezborrow\/#{_holding.sub_library.code}/ }
   it { should match /id="holding-request-option-ezborrow"/ }
-  it { should match /Request this item from E-ZBorrow \(NYU only\)/ }
-  it { should match /Requests arrive within 3-5 days./ }
+  it { should match /Search E-ZBorrow for this item./ }
+  it { should match /If available to request, the item should arrive in 3-5 days for 12 week loan./ }
 end
