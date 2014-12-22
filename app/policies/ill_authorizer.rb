ce648064ffa3a71eb573757255c2bc74908b2168
B Alter <barnaby.alter@gmail.com>
2014-12-22 14:56:31 -0500

    Updated ILLAuthorizer to work in testing

--- app/policies/ill_authorizer.rb
+++ app/policies/ill_authorizer.rb
@@ -1,6 +1,20 @@
 class ILLAuthorizer < PatronStatusAuthorizer
   def initialize(user)
     super(user)
-    @authorized_bor_statuses = Figs.env.ill_patron_statuses.map {|status| status["code"]} || {}
+    @authorized_bor_statuses = patron_statuses
+  end
+
+  private
+  # Get list of patron statuses from environment
+  def patron_statuses
+    # First check figs for hash version of patron statuses
+    if (Figs.env.ill_patron_statuses.present?)
+      Figs.env.ill_patron_statuses.map {|status| status["code"]}
+    # Then check the local environment
+    elsif ENV["ILL_PATRON_STATUSES"].present?
+      ENV["ILL_PATRON_STATUSES"].split(",")
+    else
+      []
+    end
   end
 end
