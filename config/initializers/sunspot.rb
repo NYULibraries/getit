da62961a201ab883f3864661a223ccfcdc256953
B Alter <barnaby.alter@gmail.com>
2015-03-05 14:46:35 -0500

    Fixed typo in sunspot init

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -2,7 +2,7 @@ formaggio_env_file = Rails.root.join(".formaggio-environment")
 
 if File.exists?(formaggio_env_file)
   formaggio_env = File.read(formaggio_env_file)
-  if formaggio_environment == "reindex"
+  if formaggio_env == "reindex"
     Sunspot.config.solr.url = Figs.env.reindex.solr_url
   end
 end
