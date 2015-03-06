fcdc6b2c4b96611a407b64d452829d8fc1084bd8
Barnaby <barnaby.alter@nyu.edu>
2015-03-06 14:11:56 -0500

    Fixed figs syntax for loading reindex sunspot url

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -3,6 +3,6 @@ formaggio_env_file = Rails.root.join(".formaggio-environment")
 if File.exists?(formaggio_env_file)
   formaggio_env = File.read(formaggio_env_file)
   if formaggio_env == "reindex"
-    Sunspot.config.solr.url = Figs.env.reindex.solr_url
+    Sunspot.config.solr.url = Figs.env.reindex["SOLR_URL"]
   end
 end
