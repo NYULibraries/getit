50f39d2ce1b013c0b47a7f9cb6fad1e0016c4818
B Alter <barnaby.alter@gmail.com>
2015-06-10 14:27:21 -0400

    Remove the DB seed for the development user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# Set up a development user
-require 'authlogic'
-username = 'dev123'
-if Rails.env.development? and User.find_by_username(username).nil?
-  salt = Authlogic::Random.hex_token
-  user = User.create!({
-    username: username,
-    firstname: 'Dev',
-    lastname: 'Eloper',
-    email: 'dev.eloper@library.edu',
-    password_salt: salt,
-    crypted_password: Authlogic::CryptoProviders::Sha512.encrypt(username + salt),
-    persistence_token: Authlogic::Random.hex_token,
-  })
-  user.user_attributes = {
-    nyuidn: (ENV['BOR_ID'] || 'BOR_ID'),
-    primary_institution: :NYU,
-    institutions: [:NYU],
-    bor_status: '51'
-  }
-  user.save!
-end
