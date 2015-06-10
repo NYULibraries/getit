ddd92d9a8607e68fc9d212403007e12c712ac61b
B Alter <barnaby.alter@gmail.com>
2015-06-10 14:19:49 -0400

    Remove the Authpds controller mixin from the ApplicationController; And add insitutions helper from nyulibraires-assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include Authpds::Controllers::AuthpdsController
 
-  # For dev purposes
-  def current_user_dev
-   @current_user_dev ||= User.find_by_username('dev123')
-  end
-  alias :current_user :current_user_dev if Rails.env.development?
+  # helper :institutions
+  include Nyulibraries::Assets::InstitutionsHelper
 
   def url_for_request(request)
     url_for(controller: :resolve, action: :index, only_path: false,
