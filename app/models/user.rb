b2f091f2fd10df1bb60b8d3c39afcb09b87e7587
B Alter <barnaby.alter@gmail.com>
2015-06-10 15:25:13 -0400

    Add validation for institution codes and make institution code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
+  VALID_INSTITUTION_CODES = Institutions.institutions.keys.map(&:to_s)
+
   devise :omniauthable, omniauth_providers: [:nyulibraries]
+
+  # Must have a valid institution code
+  validates :institution_code, inclusion: {in: VALID_INSTITUTION_CODES},
+    allow_blank: true
+
+  # Resolve the institution based on the institution_code
+  def institution
+    unless institution_code.blank?
+      @institution ||= Institutions.institutions[institution_code.to_sym]
+    end
+  end
 
 end
