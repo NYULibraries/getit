107c13966a5c8cf1f4d12a3356afaaa4ec1c8736
B Alter <barnaby.alter@gmail.com>
2015-09-11 15:43:38 -0400

    Send return_uri to login when attempting passive

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,15 @@ class ApplicationController < ActionController::Base
   end
 
   def passive_login_url
-    "#{ENV['LOGIN_URL']}#{ENV['PASSIVE_LOGIN_PATH']}?client_id=#{ENV['APP_ID']}"
+    "#{ENV['LOGIN_URL']}#{ENV['PASSIVE_LOGIN_PATH']}?client_id=#{ENV['APP_ID']}&origin=#{request_url_escaped}&return_uri=#{request_url_escaped}&login_path=#{login_path_escaped}"
+  end
+
+  def request_url_escaped
+    CGI::escape(request.url)
+  end
+
+  def login_path_escaped
+    CGI::escape("#{Rails.application.config.action_controller.relative_url_root}/login")
   end
 
 end
