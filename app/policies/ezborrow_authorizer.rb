9f27c7fe1f3db7124d2b55bd239016b53433b958
B Alter <barnaby.alter@gmail.com>
2015-12-17 15:17:02 -0500

    Add TNS patron statuses to EZBorrow authorizer

--- app/policies/ezborrow_authorizer.rb
+++ app/policies/ezborrow_authorizer.rb
@@ -1,6 +1,6 @@
 class EZBorrowAuthorizer < PatronStatusAuthorizer
   def initialize(user)
     super(user)
-    @authorized_bor_statuses = %w{20 21 22 23 50 51 52 53 54 55 56 57 58 60 61 62 63 65 66 80 81 82}
+    @authorized_bor_statuses = %w{20 21 22 23 50 51 52 53 54 55 56 57 58 60 61 62 63 65 66 80 81 82 30 31 32 33 34 35 36 37 38 39 40 41}
   end
 end
