32e2bffdae32345947213a0e091f434b9e9637c5
B Alter <barnaby.alter@gmail.com>
2015-12-18 16:44:44 -0500

    Allow user to make requests when holding is only available for ezborrow

--- app/policies/holding_request/authorizer.rb
+++ app/policies/holding_request/authorizer.rb
@@ -10,11 +10,11 @@ class HoldingRequest
     end
 
     # Status is requestable if the Holding is in an
-    # ILL state or the combination of User and Holding
-    # is requestable
+    # ILL state, Ezborrow state or the combination
+    # of User and Holding is requestable
     def requestable?
       (ill? || available? || recallable? ||
-        processing? || on_order? || offsite?)
+        processing? || on_order? || offsite? || ezborrow?)
     end
 
     def ezborrow?
@@ -30,7 +30,8 @@ class HoldingRequest
     end
 
     def recallable?
-      (holding.checked_out? || holding.requested?) && privileges.hold_request? && !ill? && !ezborrow?
+      (holding.checked_out? || holding.requested?) && privileges.hold_request? &&
+        !ill? && !ezborrow?
     end
     alias_method :recall?, :recallable?
 
