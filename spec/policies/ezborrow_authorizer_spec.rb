a0e1306ef6e07f1d0ecb5b93188a44323162e627
B Alter <barnaby.alter@gmail.com>
2015-12-18 13:54:26 -0500

    Update specs to look for NS ezborrow script

--- spec/policies/ezborrow_authorizer_spec.rb
+++ spec/policies/ezborrow_authorizer_spec.rb
@@ -10,7 +10,7 @@ describe EZBorrowAuthorizer do
   end
   describe '#authorized_bor_statuses' do
     subject { authorizer.authorized_bor_statuses }
-    it { should eq %w{20 21 22 23 50 51 52 53 54 55 56 57 58 60 61 62 63 65 66 80 81 82} }
+    it { should eq %w{20 21 22 23 50 51 52 53 54 55 56 57 58 60 61 62 63 65 66 80 81 82 30 31 32 33 34 35 36 37 38 39 40 41} }
   end
   describe '#authorized?' do
     subject { authorizer.authorized? }
