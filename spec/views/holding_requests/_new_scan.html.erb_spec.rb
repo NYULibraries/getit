1053f6290d8fc9bad1cd431ef362a753b3f92bbe
B Alter <barnaby.alter@gmail.com>
2015-12-23 12:18:28 -0500

    Updated cukes and specs for scanning

--- spec/views/holding_requests/_new_scan.html.erb_spec.rb
+++ spec/views/holding_requests/_new_scan.html.erb_spec.rb
@@ -10,14 +10,6 @@ describe 'holding_requests/_new_scan', vcr: {cassette_name: 'holding requests'} 
     render '/holding_requests/new_scan'
   end
   it { should match /id="holding-request-scan"/ }
-  it { should match /name="type"/ }
-  it { should match /value="available"/ }
-  it { should match /id="holding-request-option-available-entire"/ }
-  it { should match /for="entire_yes"/ }
-  it { should match /id="entire_yes"/ }
-  it { should match /Request this item to be delivered to the pickup location of your choice./ }
-  it { should match /name="pickup_location"/ }
-  it { should match /id="holding-request-option-available-scan"/ }
   it { should match /for="entire_no"/ }
   it { should match /id="entire_no"/ }
   it { should match /Request that a portion of the item be scanned and delivered electronically./ }
