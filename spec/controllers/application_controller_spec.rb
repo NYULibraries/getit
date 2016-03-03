4bd79811e8d85498309dec10c36998eb579601ed
B Alter <barnaby.alter@gmail.com>
2016-03-03 12:39:11 -0500

    Upping test coverage for new routing error functions and passive login

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -6,8 +6,69 @@ describe ApplicationController  do
     it { should eq "http://test.host/resolve?umlaut.request_id=#{request.id}"}
   end
 
+  describe '#logout_path' do
+    subject { controller.send(:logout_path) }
+    it { should eql 'https://dev.login.library.nyu.edu/logout' }
+  end
+
+  describe '#passive_login_url' do
+    subject { controller.send(:passive_login_url) }
+    it { should include 'https://dev.login.library.nyu.edu/login/passive?client_id=' }
+    it { should include '&return_uri=http%3A%2F%2Ftest.host' }
+  end
+
+  describe '#request_url_escaped' do
+    subject { controller.send(:request_url_escaped) }
+    it { should eql 'http%3A%2F%2Ftest.host' }
+  end
+
+  describe '#new_session_path' do
+    subject { controller.send(:new_session_path, :user) }
+    it { should eql '/login' }
+  end
+
+  describe '#after_sign_in_path_for' do
+    subject { controller.send(:after_sign_in_path_for, :user) }
+    it { should eql '/' }
+  end
+
+  describe '#after_sign_out_path_for' do
+    subject { controller.send(:after_sign_out_path_for, :user) }
+    context 'when LOGIN_URL is set in environment' do
+      it { should eql 'https://dev.login.library.nyu.edu/logout' }
+    end
+    context 'when LOGIN_URL is null' do
+      before { stub_const('ENV', ENV.to_hash.merge('LOGIN_URL' => nil)) }
+      it { should eql '/' }
+    end
+  end
+
   describe '#institution_param' do
     subject { controller.send(:institution_param) }
     it { should eql nil }
   end
+
+  describe '#routing_error' do
+    let(:format) { nil }
+    let(:path) { 'hack' }
+    before { get :routing_error, path: path, format: format }
+    subject { response.body }
+    context 'when format is not specified' do
+      it { should render_template('errors/404') }
+    end
+    context 'when format is JS' do
+      let(:format) { 'js' }
+      it { should eql '{"file":"/hack.js","error":"does not exist"}' }
+    end
+    context 'when format is JSON' do
+      let(:format) { 'json' }
+      it { should eql '{"file":"/hack.json","error":"does not exist"}' }
+    end
+    context 'when format is XML' do
+      let(:format) { 'xml' }
+      it { should include '<file>/hack.xml</file>' }
+      it { should include '<error>does not exist</error>' }
+    end
+  end
+
 end
