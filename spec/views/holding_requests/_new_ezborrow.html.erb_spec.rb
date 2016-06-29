6802c41c657e26586dc2ec6db81678b8f2da176a
barnabyalter <barnaby.alter@nyu.edu>
2016-06-29 16:33:49 -0400

    Made more robust tests for checking holding request options as a signed in user

--- spec/views/holding_requests/_new_ezborrow.html.erb_spec.rb
+++ spec/views/holding_requests/_new_ezborrow.html.erb_spec.rb
@@ -6,11 +6,18 @@ describe 'holding_requests/_new_ezborrow', vcr: {cassette_name: 'holding request
   let(:_holding_request) { HoldingRequest.new(_holding, _user) }
   subject { rendered }
   before do
+    allow(view).to receive(:current_user).and_return(_user)
     assign(:holding_request, _holding_request)
     render '/holding_requests/new_ezborrow'
   end
   it { should match /\/holding_requests\/#{_service_response.id}\/ezborrow\/#{_holding.sub_library.code}/ }
   it { should match /id="holding-request-option-ezborrow"/ }
   it { should match /Search E-ZBorrow for this item./ }
-  it { should match /If available to request, the item should arrive at Bobst Library within 3-5 business days for a twelve-week loan. Please allow additional transit time if you select another library as your pickup location./ }
+  context 'when logged in user is an NYU user' do
+    it { should match /If available to request, the item should arrive at Bobst Library within 3-5 business days for a twelve-week loan. Please allow additional transit time if you select another library as your pickup location./ }
+  end
+  context 'when logged in users in a New School user' do
+    let(:_user) { build(:ns_user) }
+    it { should match /If available to request, the item should arrive at your selected pickup location within 3-5 business days for a twelve-week loan./ }
+  end
 end
