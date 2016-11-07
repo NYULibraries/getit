5fdd8482a7d6fabd737da6737ac39642944d34f8
Barnaby Alter <barnaby.alter@nyu.edu>
2016-11-07 12:32:05 -0500

    Fix EZB production always pointing to borrow direct api endpoint instead of ezborrow

--- config/initializers/ezborrow.rb
+++ config/initializers/ezborrow.rb
@@ -2,3 +2,8 @@ require 'borrow_direct'
 BorrowDirect::Defaults.api_base = ENV['EZBORROW_API_BASE']
 BorrowDirect::Defaults.partnership_id = ENV['EZBORROW_PARTNERSHIP_ID']
 BorrowDirect::Defaults.timeout = 60
+# Override default production api base because engine config makes the above
+# not work in a production environment
+class BorrowDirect::Defaults
+  PRODUCTION_API_BASE = ENV['EZBORROW_API_BASE']
+end
