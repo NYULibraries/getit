56d95112de9411b3920c662d56809c6a83703686
Barnaby Alter <barnaby.alter@nyu.edu>
2017-01-11 16:04:20 -0500

    Add env specific institution files to keep tab environment flow working

--- config/initializers/institutions.rb
+++ config/initializers/institutions.rb
@@ -1,2 +1,4 @@
 Institutions.filenames << 'umlaut_services.yml'
 Institutions.filenames << 'ip_addresses.yml' if ENV.has_key?('INSTITUTIONS')
+Institutions.filenames << 'institutions.dev.yml' if Rails.env.development?
+Institutions.filenames << 'institutions.qa.yml' if Rails.env.qa?
