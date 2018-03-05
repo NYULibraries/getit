58584672ae41eae0d76d4f5dda3906ab7dffdba1
BAlter <barnaby.alter@nyu.edu>
2018-03-05 17:12:31 -0500

    Run phantomjs in a separate container and point capybara to it

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -29,9 +29,9 @@ else
     window_size: [1280, 1024],
     timeout: 500,
     debug: false,
-    js_errors: false
-    #port: 8910,
-    #host: 'http://phantomjs'
+    js_errors: false,
+    port: 8910,
+    host: 'http://phantomjs'
     )
   end
   Capybara.default_driver    = :poltergeist
