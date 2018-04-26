a67dbca9b1520346b119a408d3f0d0d27ff4c09f
BAlter <barnaby.alter@nyu.edu>
2018-04-26 13:22:44 -0400

    Don't require figs dependency on test

--- config/application.rb
+++ config/application.rb
@@ -4,8 +4,10 @@ require 'rails/all'
 
 # Load the figs variables before the rest of the bundle
 # so we can use env vars in other gems
-require 'figs'
-Figs.load(stage: Rails.env)
+unless Rails.env.test?
+  require 'figs'
+  Figs.load(stage: Rails.env)
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
