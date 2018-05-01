e26c0399598ca7bbbe673f17965fdc28c3a7d459
BAlter <barnaby.alter@nyu.edu>
2018-05-01 17:03:09 -0400

    Hack to get aroun newrelic failure

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,4 @@ set :app_title, 'getit'
 set :recipient, 'lib-getit-admin@nyu.edu'
 set :stages, ['staging', 'qa', 'production', 'reindex']
 set :rvm_ruby_string, "ruby-2.3.6"
+set :new_relic_environments, ["none"]
