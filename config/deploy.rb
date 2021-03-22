03b85918b6eaf5d35db45325238828dc1803b157
BAlter <barnaby.alter@nyu.edu>
2021-03-22 12:07:53 -0400

    Move multistage to be before default_attributes and reorder the rest

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,14 @@
+require 'formaggio/capistrano/multistage'
 require 'formaggio/capistrano/default_attributes'
 require 'formaggio/capistrano/figs'
 require 'formaggio/capistrano/config'
 require 'formaggio/capistrano/assets'
 require 'formaggio/capistrano/bundler'
-require 'formaggio/capistrano/cache'
-require 'formaggio/capistrano/multistage'
 require 'formaggio/capistrano/rvm'
-require 'formaggio/capistrano/environment'
 require 'formaggio/capistrano/server/passenger'
+require 'formaggio/capistrano/cache'
+require 'formaggio/capistrano/cleanup'
+require 'formaggio/capistrano/environment'
 
 set :app_title, 'getit'
 set :stages, ['staging', 'qa', 'production', 'reindex']
@@ -15,3 +16,5 @@ set :rvm_ruby_string, "ruby-2.5.5"
 set :new_relic_environments, ["none"]
 set :scm, :git
 set(:branch, (ENV["GIT_COMMIT"] || ENV["GIT_BRANCH"]).gsub(/remotes\//,"").gsub(/origin\//,""))
+
+
